166b80232bcf3e0cc94633b7ff07377ea68277fd
o2sa <o2sama1@outlook.sa>
2025-02-11 23:29:50 +0300

    initialize project setup for spring boot

---
+++ backend/src/main/java/com/example/problem_register/ProblemRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.problem_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemRegisterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/problem_register/ProblemRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.problem_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
